5c0175915339ff5bbc1584d3d43b5119036ad94c
Ogaranya <enomahog@gmail.com>
2022-12-24 22:28:12 +0100

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,12 +8,10 @@ import { GrClose } from "react-icons/gr";
 import SNavbar from "../styles/styled-components/SNavbar";
 
 const Navbar = () => {
-  const [showDestinations, setShowDestinations] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [showNav, setShowNav] = useState(false);
 
-  const links = ["Home", "Destination", "Services", "About", "Contact"];
-  const destinations = ["destination 1", "destination 2", "destination 3"];
+  const links = ["Home", "Services", "About", "Contact"];
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -27,145 +25,33 @@ const Navbar = () => {
 
   return (
     <SNavbar className={scrolled ? "navbar scrolled" : "navbar"}>
-      <div className="nav-container">
-        <div className="head">
-          <Link href="/">
-            <div className="logo">COVENTRY HOTELL</div>
-          </Link>
+      <div className="head">
+        <Link href="/">
+          <div className="logo">COVENTRY HOTELL</div>
+        </Link>
 
-          <button className="hamburger" onClick={() => setShowNav(true)}>
-            <GiHamburgerMenu color="#fff" size={30} />
-          </button>
-        </div>
+        <button className="hamburger" onClick={() => setShowNav(true)}>
+          {showNav ? <GrClose color="#fff" size={30} className="close"/> : <GiHamburgerMenu color="#fff" size={30} />}
+        </button>
+      </div>
 
-        <ul className="ul">
+      <div className={showNav ? "modal show" : "modal"} onClick={() => setShowNav(false)}></div>
+      <nav className={showNav ? "show" : ""}>
+        <ul>
           {links.map((link) => {
-            if (link === "Destination") {
-              return (
-                <div className="destinations" key={link}>
-                  <button
-                    className="destination"
-                    onClick={() => setShowDestinations((prev) => !prev)}
-                  >
-                    {link}{" "}
-                    <RiArrowDropDownLine
-                      size={25}
-                      className={showDestinations ? "rotate" : ""}
-                    />
-                  </button>
-
-                  <ul
-                    className={
-                      showDestinations
-                        ? "destinations-ul show"
-                        : "destinations-ul"
-                    }
-                  >
-                    {destinations.map((destination) => (
-                      <li key={destination}>
-                        <button>{destination}</button>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              );
-            } else {
-              return (
-                <li key={link}>
-                  <Link
-                    href={
-                      link === "Home" ? "/" : `/${link.toLocaleLowerCase()}`
-                    }
-                    className="nav"
-                  >
-                    {link}
-                  </Link>
-                </li>
-              );
-            }
+            return (
+              <li key={link}>
+                <Link
+                  href={link === "Home" ? "/" : `/${link.toLocaleLowerCase()}`}
+                  className="nav"
+                >
+                  {link}
+                </Link>
+              </li>
+            );
           })}
         </ul>
-
-        <div className="tel">
-          <span className="phone">
-            <BsFillTelephoneFill />
-          </span>
-
-          <span className="no">+123-489-9381</span>
-        </div>
-
-        <div className="mobile">
-          <button className="open" onClick={() => setShowNav(true)}>
-            <GiHamburgerMenu color="#fff" size={30} />
-          </button>
-
-          <div
-            className={showNav ? "modal show-modal" : "modal"}
-            onClick={() => setShowNav(false)}
-          ></div>
-
-          <ul className={showNav ? "mobile-ul show-nav" : "mobile-ul"}>
-            <button className="close" onClick={() => setShowNav(false)}>
-              <GrClose size={30} />
-            </button>
-
-            {links.map((link) => {
-              if (link === "Destination") {
-                return (
-                  <div className="destinations" key={link}>
-                    <button
-                      className="destination"
-                      onClick={() => setShowDestinations((prev) => !prev)}
-                    >
-                      {link}{" "}
-                      <RiArrowDropDownLine
-                        size={30}
-                        className={showDestinations ? "rotate" : ""}
-                      />
-                    </button>
-
-                    <ul
-                      className={
-                        showDestinations
-                          ? "destinations-ul show"
-                          : "destinations-ul"
-                      }
-                    >
-                      {destinations.map((destination) => (
-                        <li key={destination}>
-                          <button>{destination}</button>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                );
-              } else {
-                return (
-                  <li key={link}>
-                    <Link
-                      href={
-                        link === "Home" ? "/" : `/${link.toLocaleLowerCase()}`
-                      }
-                      className="nav"
-                      onClick={() => setShowNav(false)}
-                    >
-                      {link}
-                    </Link>
-                  </li>
-                );
-              }
-            })}
-
-            <Link href="#" className="tel tel-mobile">
-              <span className="phone">
-                <BsFillTelephoneFill />
-              </span>
-
-              <span className="no">+123-489-9381</span>
-            </Link>
-          </ul>
-        </div>
-      </div>
+      </nav>
     </SNavbar>
   );
 };
