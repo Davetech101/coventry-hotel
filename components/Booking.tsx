1502e44b13e9423afcffe26716e4ab85175b7413
Ogaranya <enomahog@gmail.com>
2022-12-27 06:48:38 +0100

    booking sec

---
+++ components/Booking.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SBooking from "../styles/styled-components/SBooking"
+
+const Booking = () => {
+  return (
+    <SBooking>
+      <form>
+        <input type="date" />
+        <input type="date" name="" id="" />
+        <button>Book Now</button>
+      </form>
+    </SBooking>
+  );
+};
+
+export default Booking;
