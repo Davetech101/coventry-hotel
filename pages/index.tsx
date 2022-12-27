f1d36d63cdbd10752f92adb4fbf420d0b26d541e
Ogaranya <enomahog@gmail.com>
2022-12-27 06:47:46 +0100

    booking sec

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
+import Booking from '../components/Booking'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
      <Navbar/>
+     <Booking/>
     </>
   )
 }
