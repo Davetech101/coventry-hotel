ed24ab190b6f0cad4d791159db56147ed24c9a8b
Ogaranya <enomahog@gmail.com>
2022-12-28 06:00:04 +0100

    booking sec

--- styles/styled-components/SBooking.tsx
+++ styles/styled-components/SBooking.tsx
@@ -1,7 +1,15 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 
 const SBooking = styled.section`
-display:flex;
-`
-export default SBooking
+  display: flex;
+  flex-direction: column;
+  align-items:center;
+  justify-content:center;
+  padding:5rem 0;
+
+  input {
+    display: block;
+    padding:2rem;
+  }
+`;
+export default SBooking;
