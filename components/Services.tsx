0e92fba7778604e654b2a9965e682dae3652f037
Ogaranya <enomahog@gmail.com>
2022-12-30 07:34:16 +0100

    services

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import SServices from "../styles/styled-components/SServices";
 
-import { FaWalking } from "react-icons/fa";
+import {
+  FaWalking,
+  GiTreasureMap,
+  RiSuitcase3Fill,
+  GiIsland,
+  GiWorld,
+  GiAirplaneDeparture,
+} from "react-icons/all";
 
 const Services = () => {
   const arr: { name: string; age: number }[] = [
@@ -9,49 +16,53 @@ const Services = () => {
     { name: "Bob", age: 28 },
     { name: "Carl", age: 29 },
   ];
-  const services: { title: string; txt: string; icon: string }[] = [
+  const services = [
     {
-      icon: "FaWalking",
+      icon: <FaWalking />,
       title: "Trekking",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
     {
-      icon: "",
+      icon: <GiTreasureMap />,
       title: "The Map",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
     {
-      icon: "",
+      icon: <RiSuitcase3Fill />,
       title: "Suitcase",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
     {
-      icon: "",
+      icon: <GiIsland />,
       title: "Island Hoping",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
     {
-      icon: "",
+      icon: <GiWorld />,
       title: "World Round",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
     {
-      icon: "",
+      icon: <GiAirplaneDeparture />,
       title: "Travel with Plane",
       txt: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     },
   ];
-  const wk = "FaWalking";
 
   return (
     <SServices>
       <h2>Our Services</h2>
 
-      <main>{services.map((service, idx) => (
-        <div key={idx}>
-            
-        </div>
-      ))}</main>
+      <main>
+        {services.map((service, idx) => (
+          <div key={idx}>
+            {service.icon}
+            <h3>{service.title}</h3>
+
+            <p>{service.txt}</p>
+          </div>
+        ))}
+      </main>
     </SServices>
   );
 };
