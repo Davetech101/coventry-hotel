a9153c81e5a9ccbd4179c5ea85bea7d921654181
Ogaranya <enomahog@gmail.com>
2023-01-01 01:44:17 +0100

    faq

--- styles/styled-components/SFaq.tsx
+++ styles/styled-components/SFaq.tsx
@@ -2,15 +2,26 @@ import styled from "styled-components";
 
 const SFaq = styled.section`
   padding: 3rem;
-  background:#1b361b;
-  color:#fff;
+  background: #1b361b;
+  color: #f2f2f2e5;
 
   h2 {
     font-size: 3rem;
-    color: #fff;
+    color: #f2f2f2e5;
     margin-bottom: 3rem;
     text-transform: uppercase;
   }
-  
+
+  details {
+    font-size: 1.5rem;
+    padding: 1rem;
+    border-bottom: 1px solid;
+    background: #042604;
+  }
+
+  summary {
+    background: #1b361b;
+    padding:1rem;
+  }
 `;
 export default SFaq;
