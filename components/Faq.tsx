841949d669278c1f488ed83b1a538bb921ed210b
Ogaranya <enomahog@gmail.com>
2023-01-01 01:21:46 +0100

    faq

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import SFaq from '../styles/styled-components/SFaq'
+import React from "react";
+import SFaq from "../styles/styled-components/SFaq";
 
 const Faq = () => {
   return (
     <SFaq>
-        <h2>
-            Frequently asked questions
-        </h2>
-    </SFaq>
-  )
-}
+      <h2>Frequently asked questions</h2>
 
-export default Faq
+      <details>
+        <summary>Is it free?</summary>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos
+        debitis dignissimos provident nulla vero quae nesciunt cum autem quam?
+      </details>
+      
+      <details>
+        <summary>How to install this template?</summary>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos
+        debitis dignissimos provident nulla vero quae nesciunt cum autem quam?
+      </details>
+
+      <details>
+        <summary>Where can I get help?</summary>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos
+        debitis dignissimos provident nulla vero quae nesciunt cum autem quam?
+      </details>
+    </SFaq>
+  );
+};
+
+export default Faq;
--- styles/styled-components/SFaq.tsx
+++ styles/styled-components/SFaq.tsx
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 const SFaq = styled.section`
   padding: 3rem;
+  background:#1b361b;
+  color:#fff;
+
   h2 {
     font-size: 3rem;
-    color: #1b361b;
+    color: #fff;
     margin-bottom: 3rem;
     text-transform: uppercase;
   }
+  
 `;
 export default SFaq;
