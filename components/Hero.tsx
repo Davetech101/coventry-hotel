7599337acf46f1c0d7cb46f8bb69c875b4c6634d
Ogaranya <enomahog@gmail.com>
2023-01-08 16:55:57 +0100

    hero

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SNavbar from '../styles/styled-components/SNavbar'
+
+const Hero = () => {
+  return (
+    <SNavbar>
+        
+    </SNavbar>
+  )
+}
+
+export default Hero
