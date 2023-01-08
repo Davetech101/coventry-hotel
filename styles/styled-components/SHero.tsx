9ff4860fb6e46fc037de901928bccb9c24da4ca9
Ogaranya <enomahog@gmail.com>
2023-01-08 17:01:59 +0100

    hero

---
+++ styles/styled-components/SHero.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+const SHero = styled.main`
+  background-image: linear-gradient(
+      to left,
+      rgba(0, 0, 0, 0.6),
+      rgba(0, 0, 0, 0.6)
+    ),
+    url(/assets/Hero.jpg);
+  background-size: cover;
+  background-position: left;
+  background-repeat: no-repeat;
+  min-height: 90vh;
+  position: relative;
+`
+
+export default SHero;
