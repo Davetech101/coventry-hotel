a99e5a404a4d679f96fddf3dd36516db114e8526
Ogaranya <enomahog@gmail.com>
2023-01-08 17:39:08 +0100

    services page

--- pages/services.tsx
+++ pages/services.tsx
@@ -1,7 +1,13 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
 export default function OurServices() {
   return (
     <>
-     
+     <Navbar/>
+     <Hero/>
+     <Footer/>
     </>
   )
 }
