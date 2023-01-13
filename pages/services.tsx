e8d8a9f70b33811ce652d1cb6ac7b47fd4dddf12
Ogaranya <enomahog@gmail.com>
2023-01-13 08:59:58 +0100

    hero

--- pages/services.tsx
+++ pages/services.tsx
@@ -5,9 +5,13 @@ import Navbar from "../components/Navbar";
 export default function OurServices() {
   return (
     <>
-     <Navbar/>
-     <Hero text="Let the"/>
-     <Footer/>
+      <Navbar />
+      <Hero
+        text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+        title="Our Services"
+        type="services"
+      />
+      <Footer />
     </>
-  )
+  );
 }
