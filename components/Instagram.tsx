a8775f271338245b3ace6749158577b0e0a8c301
Ogaranya <enomahog@gmail.com>
2023-01-17 20:21:45 +0100

    ig

---
+++ components/Instagram.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SInstagram from '../styles/styled-components/SInstagram'
+
+const Instagram = () => {
+  return (
+    <SInstagram>
+
+    </SInstagram>
+  )
+}
+
+export default Instagram
---
+++ styles/styled-components/SInstagram.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const SInstagram = styled.section`
+  
+`;
+
+export default SInstagram;
