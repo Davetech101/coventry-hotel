94edfd4454603381aa39b39f8bef62c40018d968
Ogaranya <enomahog@gmail.com>
2023-01-17 21:52:58 +0100

    ig

--- components/Instagram.tsx
+++ components/Instagram.tsx
@@ -6,7 +6,11 @@ const Instagram = () => {
     <SInstagram>
       <h2>Instagram</h2>
 
-      
+      <p>
+        A small river named Duden flows by their place and supplies it with the
+        necessary regelialia. It is a paradisematic country, in which roasted
+        parts of sentences fly into your mouth.
+      </p>
     </SInstagram>
   );
 };
