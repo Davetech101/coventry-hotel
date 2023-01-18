4382ad2de11ca363b0772060399acd9a995aeda4
Ogaranya <enomahog@gmail.com>
2023-01-18 20:01:33 +0100

    ig

--- styles/styled-components/SInstagram.tsx
+++ styles/styled-components/SInstagram.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const SInstagram = styled.section`
-  padding:3rem 15rem;
+  padding: 3rem 15rem;
+
+  h2 {
+    padding: 3rem;
+  }
 `;
 
 export default SInstagram;
