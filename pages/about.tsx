487477c4d3c8d7da7d81857620726c2364b46625
Ogaranya <enomahog@gmail.com>
2023-01-19 19:21:58 +0100

    about

---
+++ pages/about.tsx
@@ -0,0 +1,25 @@
+import Brusque from "../components/Brusque";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Instagram from "../components/Instagram";
+import Navbar from "../components/Navbar";
+import Services from "../components/Services";
+import WhyUs from "../components/WhyUs";
+
+export default function about() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+        title="Our Services"
+        type=""
+        bgImg="serviceHero"
+      />
+      <Services/>
+      <WhyUs/>
+      <Instagram/>
+      <Footer />
+    </>
+  );
+}
