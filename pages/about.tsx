bb1503829de8b9d1e3d82178bd82ae692b7fe6b5
Ogaranya <enomahog@gmail.com>
2023-01-19 19:44:02 +0100

    about

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,7 +12,7 @@ export default function about() {
       <Navbar />
       <Hero
         text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
-        title="Our Services"
+        title="About us"
         type=""
         bgImg="serviceHero"
       />
