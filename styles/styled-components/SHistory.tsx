188d38e71c1435479628253f071d8da5d37ea995
Ogaranya <enomahog@gmail.com>
2023-01-20 03:27:57 +0100

    add history

---
+++ styles/styled-components/SHistory.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SHistory = styled.section`
+  padding: 3rem 15rem;
+
+  h2 {
+    font-size: 3rem;
+    color: #1b361b;
+  }
+`;
+
+export default SHistory;
