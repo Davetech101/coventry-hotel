c7dad3f254b1963c096aeca58b68583e18857d3d
Ogaranya <enomahog@gmail.com>
2023-01-23 15:59:48 +0100

    wwa

--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import SWwa from '../styles/styled-components/SWwa'
 
 const WhoWeAre = () => {
+  const arr = [
+    {
+      
+    }
+  ]
   return (
+
     <SWwa>
       <h2>
         Who we are
       </h2>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendi iure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.
+      </p>
     </SWwa>
   )
 }
