50d114f07d430eb1600a2ef40be0c729f8c3f428
Ogaranya <enomahog@gmail.com>
2023-01-23 16:01:02 +0100

    wwa

--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -1,26 +1,55 @@
-import React from 'react'
-import SWwa from '../styles/styled-components/SWwa'
+import React from "react";
+import SWwa from "../styles/styled-components/SWwa";
 
 const WhoWeAre = () => {
   const arr = [
     {
       name: "",
       position: "",
-      txt: ""
+      txt: "",
     },
-  ]
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+    {
+      name: "",
+      position: "",
+      txt: "",
+    },
+  ];
   return (
-
     <SWwa>
-      <h2>
-        Who we are
-      </h2>
+      <h2>Who we are</h2>
 
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendi iure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum
+        quisquam debitis facere deserunt tempore optio laborum rerum eligendi
+        iure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.
       </p>
     </SWwa>
-  )
-}
+  );
+};
 
-export default WhoWeAre
+export default WhoWeAre;
