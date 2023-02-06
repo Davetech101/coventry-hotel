4a24f8bf5f2bfaae4dc40ac345f974b601281aad
Ogaranya <enomahog@gmail.com>
2023-02-06 19:44:25 +0100

    wwa

--- components/WhoWeAre.tsx
+++ components/WhoWeAre.tsx
@@ -1,30 +1,34 @@
-import { profile } from "console";
 import React from "react";
 import SWwa from "../styles/styled-components/SWwa";
 
 const WhoWeAre = () => {
   const profiles = [
     {
+      img: "../public/assets/james.jpg",
       name: "James Scott",
       position: "CEO, Co-Founder",
       txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendiure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.",
     },
     {
+      img: "../public/assets/hannah.jpg",
       name: "Hannah White",
       position: "CEO, Co-Founder",
       txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendiure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.",
     },
     {
+      img: "../public/assets/amy.jpg",
       name: "Amy Perez",
       position: "CEO, Co-Founder",
       txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendiure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.",
     },
     {
+      img: "../public/assets/brian.jpg",
       name: "Brian Nance",
       position: "CEO, Co-Founder",
       txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendiure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.",
     },
     {
+      img: "../public/assets/ahmed.jpg",
       name: "Ahmed Kenney",
       position: "CEO, Co-Founder",
       txt: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos earum quisquam debitis facere deserunt tempore optio laborum rerum eligendiure hic sint, ullam impedit similique illo nihil, vitae suscipit neque.",
@@ -35,20 +39,21 @@ const WhoWeAre = () => {
       <h2>Who we are</h2>
 
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum velit hic voluptatem neque optio recusandae distinctio ea quidem odio sint delectus laboriosam, excepturi omnis minus necessitatibus? Tempore dolores aut blanditiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum velit hic
+        voluptatem neque optio recusandae distinctio ea quidem odio sint
+        delectus laboriosam, excepturi omnis minus necessitatibus? Tempore
+        dolores aut blanditiis?
       </p>
 
       <main>
-          {profiles.map(profile => (
-            <div key={profile.name}>
-              <h4>
-               {profile.name}
-              </h4>
-              <small>{profile.position}</small>
-              <p>{profile.txt}</p>
-              <button>Learn More</button>
-            </div>
-          ))}
+        {profiles.map((profile) => (
+          <div key={profile.name}>
+            <h4>{profile.name}</h4>
+            <small>{profile.position}</small>
+            <p>{profile.txt}</p>
+            <button>Learn More</button>
+          </div>
+        ))}
       </main>
     </SWwa>
   );
