fa24411d04873c0977f0066b0573c63de594487a
Ogaranya <enomahog@gmail.com>
2023-02-06 18:53:31 +0100

    wwa

--- styles/styled-components/SWwa.tsx
+++ styles/styled-components/SWwa.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const SWwa = styled.section`
   padding: 5rem 15rem;
+  background: #f8f9fa;
 
   h2 {
     font-size: 3rem;
@@ -11,6 +12,10 @@ const SWwa = styled.section`
   > p{
     font-size: 1.8rem;
     margin:2rem 0;
+  }
+
+  main > div {
+    background: #fff;
   }
 `;
 
