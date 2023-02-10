12b9545905f03b3e780c73442cd56d8c1732ba49
Ogaranya <enomahog@gmail.com>
2023-02-10 21:02:32 +0100

    get in touch

--- components/GetInTouch.tsx
+++ components/GetInTouch.tsx
@@ -4,8 +4,13 @@ import SGetInTouch from "../styles/styled-components/SGetInTouch";
 const GetInTouch = () => {
   return (
     <SGetInTouch>
+        <h2>Get in touch</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates autem, repellat soluta perferendis cumque omnis maxime eaque. Et vitae ut modi, aliquid laudantium eaque fugit beatae quis excepturi totam ab.</p>
       <form>
-        
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
       </form>
     </SGetInTouch>
   );
