725d794f21f2d90ce045ae473485d4ac35d94d75
Ogaranya <enomahog@gmail.com>
2023-02-10 20:20:48 +0100

    contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import History from "../components/History";
+import Navbar from "../components/Navbar";
+import WhoWeAre from "../components/WhoWeAre";
+
+export default function about() {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        text="Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts."
+        title="About us"
+        type=""
+        bgImg="aboutHero"
+      />
+      <History/>
+      <WhoWeAre/>
+      <Footer />
+    </>
+  );
+}
