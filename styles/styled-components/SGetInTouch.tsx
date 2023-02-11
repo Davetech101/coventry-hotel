2b3431d01f418038f2c87d8022c68be75c90ff88
Ogaranya <enomahog@gmail.com>
2023-02-11 19:29:36 +0100

    get in touch

--- styles/styled-components/SGetInTouch.tsx
+++ styles/styled-components/SGetInTouch.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
 const SGetInTouch = styled.section`
-    z-index:10;
-    position:relative;
+  z-index: 10;
+  position: relative;
 
-    h2{
-        
-    }
-  
+  h2 {
+    font-size: 3rem;
+    color: #1b361b;
+    margin-bottom: 3rem;
+    padding: 0;
+    text-transform: uppercase;
+  }
 `;
 
 export default SGetInTouch;
