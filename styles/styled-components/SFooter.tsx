ead5cadb24e24e2f7dec716f7f3d2f18beb3eb2e
Ogaranya <enomahog@gmail.com>
2023-02-14 17:21:00 +0100

    footer

--- styles/styled-components/SFooter.tsx
+++ styles/styled-components/SFooter.tsx
@@ -17,6 +17,7 @@ const SFooter = styled.footer`
 
   a{
     font-size: 1.4rem;
+    color: #9c9c9c;
   }
 
   .links {
@@ -41,38 +42,38 @@ const SFooter = styled.footer`
     border:1px solid #1b361b;
   }
 
-  .connect{
-    font-size:1.9rem;
-    margin-top:2rem;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-    color: #1b361b;
-  }
+  // .connect{
+  //   font-size:1.9rem;
+  //   margin-top:2rem;
+  //   display:flex;
+  //   justify-content: space-between;
+  //   align-items: center;
+  //   color: #1b361b;
+  // }
 
-  .connect button{
-    font-size:2.8rem;
-    margin:0 1rem;
-  }
+  // .connect button{
+  //   font-size:2.8rem;
+  //   margin:0 1rem;
+  // }
 
-  .connect svg{
-    color: #1b361b;
-  }
+  // .connect svg{
+  //   color: #1b361b;
+  // }
 
-  @media only screen and (min-width: 800px) {
-    .links li {
-      font-size: 2rem;
-    }
+  // @media only screen and (min-width: 800px) {
+  //   .links li {
+  //     font-size: 2rem;
+  //   }
 
-    .connect{
-      font-size:2.2rem;
-    }
+  //   .connect{
+  //     font-size:2.2rem;
+  //   }
 
-    .connect button{
-      font-size:3.8rem;
-      margin:0 1rem;
-    }
-  }
+  //   .connect button{
+  //     font-size:3.8rem;
+  //     margin:0 1rem;
+  //   }
+  // }
 `;
 
 export default SFooter;
